f7aaa3dc126ab6fd64bf5aaa8ba567c09bb67c84
Thanh Pham <tpham0123@gmail.com>
2020-05-17 03:16:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash[]
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -90,6 +91,8 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  
+  
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
